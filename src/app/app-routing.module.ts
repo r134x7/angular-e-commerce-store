d7d0b3d2c99b2db6b6b7c99414902915bb49d40b
r134x7 <r134x7@gmx.com>
2023-04-28 23:37:22 +1000

    setting up routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/", pathMatch: "full"},
+  { path: "/login", component: LoginComponent},
+  { path: "/signup", component: SignUpComponent},
+  { path: "/orderHistory", component: OrderHistoryComponent },
+  { path: "/products/:id", component: DetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
