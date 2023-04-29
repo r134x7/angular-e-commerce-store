835ed76032b9353934bddb351a62c41ae1fc9550
r134x7 <r134x7@gmx.com>
2023-04-29 23:48:06 +1000

    trying to convert JSX format html from MERN Stack version of project to Angular html templating...

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,31 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, from, map, tap } from "rxjs";
+import { ProductsService } from '../products.service';
 import { Product } from '../models';
-import { products as mockProducts } from '../mock-product';
+
+/*
+template reference
+    <div className="card px-1 py-1">
+      <Link to={`/products/${_id}`}>
+        <img
+          alt={name}
+          src={`/images/${image}`}
+        />
+        <p>{name}</p>
+      </Link>
+      <div>
+        <div>{quantity} {pluralize("item", quantity)} in stock</div>
+        <span>${price}</span>
+      </div>
+      <button onClick={addToCart}>Add to cart</button>
+    </div>
+*/
 
 @Component({
   selector: 'app-product-list',
   template: `
-    <p>
-      product-list works!
-    </p>
+      <div className="my-2">
+      <h2>Our Products:</h2>
+
+      <!-- {state.products.length ? ( -->
+        <div *ngIf="products" className="flex-row">
+          <div *ngFor="let product of products" className="card px-1 py-1"> 
+            <p>{{product.name}}</p>
+            <div>
+              <div>{{product.quantity}} in stock</div>
+              <span>{{product.price}}</span>
+            </div>
+          <!-- <button onClick={addToCart}>Add to cart</button> -->
+          </div>
+        </div>
+      </div>
   `,
   styles: [
   ]
 })
-export class ProductListComponent implements ngOninit {
+export class ProductListComponent {
 
   // product list needs to be displayed by calling obsevables
   // start with empty products to fill
   products: Product[] = [];
 
+  // dependency injection site. Becomes a singleton instance of ProductsService.
+  constructor(private productsService: ProductsService) {}
+
+  // rely on ngOnInit lifecycle hook to call getProducts
   ngOnInit(): void {
     this.getProducts();
   }
 
   // implement method to call products from mock
-  getProducts(): Observable<Product[]> {
-    this.products.getProd
+  getProducts(): void {
+    this.products = this.productsService.getProducts();
   }
 
 }
