bb972ed134a7ff654fe35f6ab45a6facd23b276d
r134x7 <r134x7@gmx.com>
2023-04-29 23:04:32 +1000

    in middle of defining product list component, creating a products service injector

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, from, map, tap } from "rxjs";
+import { Product } from '../models';
+import { products as mockProducts } from '../mock-product';
 
 @Component({
   selector: 'app-product-list',
@@ -10,6 +13,19 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ProductListComponent {
+export class ProductListComponent implements ngOninit {
+
+  // product list needs to be displayed by calling obsevables
+  // start with empty products to fill
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  // implement method to call products from mock
+  getProducts(): Observable<Product[]> {
+    this.products.getProd
+  }
 
 }
