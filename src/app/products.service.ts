d3dd856259a3589d15a5743ed900cd3f69904745
r134x7 <r134x7@gmx.com>
2023-04-29 23:12:31 +1000

    created service injector to provide the products to component

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { products } from './mock-product';
+import { Product } from './models';
 
+// provided in the root level of the app
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  // products service separates the implementation of retrieving the data from the component that uses it in the view
+
+
   constructor() { }
+
+  getProducts(): Product[] {
+    return products;
+  }
 }
