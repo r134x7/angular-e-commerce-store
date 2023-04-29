3c854085d23a071374ce24a6cb9ccb36228b0171
r134x7 <r134x7@gmx.com>
2023-04-29 22:09:34 +1000

    implemented mock category

---
+++ src/app/mock-category.ts
@@ -0,0 +1,9 @@
+import { Category } from "./models";
+
+export const categories: Category[] = [
+    { id: 1, name: 'Food' },
+    { id: 2, name: 'Household Supplies' },
+    { id: 3, name: 'Electronics' },
+    { id: 4, name: 'Books' },
+    { id: 5, name: 'Toys' }
+];  
