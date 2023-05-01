36986846ec2970005e21dfed7076c4106d3c3f14
r134x7 <r134x7@gmx.com>
2023-05-01 20:26:19 +1000

    removed commented code but still couldn't improve method

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { products } from './mock-product';
 import { Product } from './models';
-import { Observable, of, filter, BehaviorSubject } from "rxjs";
+import { Observable, of, filter, BehaviorSubject, tap } from "rxjs";
 
 // provided in the root level of the app
 @Injectable({
@@ -13,36 +13,19 @@ export class ProductsService {
   private source = new BehaviorSubject<Product[]>(products);
   currentValue = this.source.asObservable();
   
-
   constructor() { }
-
-  // getProducts(): Observable<Product[]> {
-  //   const productsCall = of(products);
-  //   return productsCall;
-  // }
-  // getProducts() {
-  //   const productsCall = this.currentValue.subscribe(elem => this.currentValue = of(products));
-  //   console.log(productsCall);
-    
-  //   return productsCall;
-  // }
-
-  // getProducts(): BehaviorSubject<Product[]> {
-  //   const productsCall = new BehaviorSubject(products);
-  //   return productsCall;
-  // }
 
   filterProducts(id: number) {
     const toFilter = products.filter(elem => elem.category.id === id);
     this.source.next(toFilter)
+    // this.currentValue.pipe(
+    //   filter((elem, index) => {
+    //   tap(elem => console.log(elem))
+    //   return elem[index].id === id})).subscribe(elem => {
+    //     tap(elem => console.log(elem))
+    //     return this.source.next(elem)
+    //   }) 
+    // this.source.next(toFilter)
   }
 
-  // filterProducts(id: number | undefined, productsLocal: Observable<Product> ): Observable<Product> {
-  //   const productsCall = 
-  //   return (id !== undefined)
-  //     ? productsLocal.pipe(
-  //     filter(elem => elem.id === id)
-  //     )
-  //     : productsLocal
-  // }
 }
