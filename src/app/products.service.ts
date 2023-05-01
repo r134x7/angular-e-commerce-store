3d1dc708aa7ade499e6a418072390280e1daa7d9
r134x7 <r134x7@gmx.com>
2023-05-02 01:17:09 +1000

    finally found a method that keeps calling from an observable to update the state on the behaviour subject

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { products } from './mock-product';
 import { Product } from './models';
-import { Observable, of, filter, BehaviorSubject, tap } from "rxjs";
+import { Observable, from, of, map, filter, BehaviorSubject, tap } from "rxjs";
 
 // provided in the root level of the app
 @Injectable({
@@ -12,12 +12,24 @@ export class ProductsService {
   // products service separates the implementation of retrieving the data from the component that uses it in the view
   private source = new BehaviorSubject<Product[]>(products);
   currentValue = this.source.asObservable();
+
+  // to act as a continous source of the original when filtering
+  private obsSource = of(products);
   
   constructor() { }
 
   filterProducts(id: number) {
-    const toFilter = products.filter(elem => elem.category.id === id);
-    this.source.next(toFilter)
+
+    this.obsSource.pipe(map(elem => elem.filter(value => value.category.id === id))).subscribe(elem => this.source.next(elem))
+    // const toFilter = products.filter(elem => elem.category.id === id);
+    // this.source.next(toFilter)
+    // this.source.next(this.source.getValue().filter(elem => elem.id === id))
+    // this.obsSource.pipe(filter((elem, index) => elem[index].category.id === id)).subscribe(elem => this.source.next(elem))
+    //.subscribe(elem => this.source.next([elem]))
+    //.pipe(map(elem => elem.filter(value => value.id === id))).subscribe(elem => this.source.next(elem))
+    // this.source.next()
+    // console.log(this.source.getValue());
+    
     // this.currentValue.pipe(
     //   filter((elem, index) => {
     //   tap(elem => console.log(elem))
